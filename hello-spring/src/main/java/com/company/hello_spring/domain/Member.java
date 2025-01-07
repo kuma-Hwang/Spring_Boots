3961e3991bfd81e4947fc75f353c6ee1442734c2
kuma-Hwang <kuuma0807@gmail.com>
2025-01-07 17:35:48 +0900

    25.1.7 수업내용

---
+++ hello-spring/src/main/java/com/company/hello_spring/domain/Member.java
@@ -0,0 +1,11 @@
+package com.company.hello_spring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private String name;
+}
